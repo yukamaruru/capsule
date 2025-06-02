17e39d8bee92b8baa38c10533bcdcccdb82201f7
yukamaru <yuun.vv07@gmail.com>
2025-06-02 21:43:51 +0900

    capsuleクラス作成

---
+++ src/capsule/Capsule.java
@@ -0,0 +1,12 @@
+package capsule;
+
+public class Capsule {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Person taro = new Person("山田太郎" , 20);
+		System.out.println(taro.name());
+
+	}
+
+}
--- src/capsule/Person.java
+++ src/capsule/Person.java
@@ -17,11 +17,6 @@ public class Person {
 	public void setName(String name) {
 		this.name = name;
 	}//setの後ろの１文字目は大文字。setの時は戻り値の型の位置にvoidと書く。今回はsetNameで名前を書き換える処理を行うだけのため戻り値なしのvoid。
-
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
+	
 
 }
