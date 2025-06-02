4b292d43ff09d68a6dd4149d0e7a86eff6218812
yukamaru <yuun.vv07@gmail.com>
2025-06-02 21:55:18 +0900

    年齢取得

--- src/capsule/Capsule.java
+++ src/capsule/Capsule.java
@@ -5,7 +5,14 @@ public class Capsule {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		Person taro = new Person("山田太郎" , 20);
-		System.out.println(taro.name());
+		
+		System.out.println(taro.getName());
+		
+		taro.setName("花子");
+		
+		System.out.println(taro.getName());
+		
+		System.out.println(taro.getAge());
 
 	}
 
--- src/capsule/Person.java
+++ src/capsule/Person.java
@@ -2,8 +2,8 @@ package capsule;
 
 public class Person {
 	
-	public String name = null;
-	public int age = 0;
+	private String name = null;
+	private int age = 0;
 	
 	public Person (String name, int age) {
 		this.name = name;
@@ -14,9 +14,15 @@ public class Person {
 		return this.name;
 	}//getの後ろの１文字目は大文字。getの時は必ずreturn。
 	
+	public int getAge() {
+		return this.age;
+	}
 	public void setName(String name) {
 		this.name = name;
 	}//setの後ろの１文字目は大文字。setの時は戻り値の型の位置にvoidと書く。今回はsetNameで名前を書き換える処理を行うだけのため戻り値なしのvoid。
 	
+	public void setAge(int age) {
+		this.age = age;
+	}
 
 }
